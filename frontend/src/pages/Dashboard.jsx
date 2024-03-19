8bae1ac35a36ec4091ed232f4fe1dd0a1abc2b34
Hilmy030120 <mhilmy827@gmail.com>
2024-03-19 18:04:55 +0700

    first commit

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from './Layout'
+import Welcome from "../components/Welcome";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Layout>
+        <Welcome/>
+      </Layout>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './Layout'
+import ProductList from '../components/ProductList'
+
+const q = () => {
+  return (
+    <Layout>
+        <ProductList/>
+    </Layout>
+  )
+}
+
+export default q
